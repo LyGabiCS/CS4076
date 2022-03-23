f6c795509fb681452e682adb71dcd6f7e9fefbaf
Gabi <95319281+LyGabiCS@users.noreply.github.com>
2022-03-23 20:46:29 +0000

    Update item.cpp

--- item.cpp
+++ item.cpp
@@ -1,47 +1,27 @@
 #include "item.h"
 
-Item::Item (string inDescription, int inWeightGrams, float inValue/**, int weaponCheck*/) {
-	description = inDescription;
-	setWeight(inWeightGrams);
-	value = inValue;
-	/**weaponCheck(isWeapon);*/
+Item::Item (string inDescription, int value, string rarity) {
+    description = inDescription;
+    setValue(value);
+    getRarity() = rarity;
 }
 
-Item::Item(string inDescription) {
-	description = inDescription;
-}
-
-void Item::setWeight(int inWeightGrams)
-{
-    if (inWeightGrams > 9999 || inWeightGrams < 0)
-       cout << "weight invalid, must be 0<weight<9999" ;
-    else
-	   weightGrams = inWeightGrams;
-}
-
-void Item::setValue(float inValue)
+void Item::setValue(int inValue)
 {
     if (inValue > 9999 || inValue < 0)
        cout << "value invalid, must be 0<value<9999" ;
     else
-	   value = inValue;
+       value = inValue;
 }
 
-/**void Item::setWeaponCheck(int isWeapon)
-{
-    if(isWeapon > 0 || isWeapon < 0)
-        cout << "Item not a weapon" ;
-    else
-        cout << "Item is a weapon" ;
-}*/
 
 string Item::getShortDescription()
 {
-	return description;
+    return description;
 }
 
-string Item::getLongDescription()
+string Item::getRarity()
 {
-	return " item(s), " + description + ".\n";
+    return rarity;
 }
 
