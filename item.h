46e1be6438cef93d60b167ab7e9419c04efeb9e6
Gabi <95319281+LyGabiCS@users.noreply.github.com>
2022-03-26 12:41:40 +0000

    Update item.h

--- item.h
+++ item.h
@@ -4,27 +4,35 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 // Each Item will have a name, value and rarity.
-// Name of each item will be used in the inventory section
 // Value of each item will be added up to the grand total
 // Rarity of each item will determine which wordle the player will do!
+// Total will be updated every time the player picks up and successfully finishes a wordle.
 
 class Item {
 private:
+
     string description;
-    int value;
+    string fullDescription;
+    int value = 0;
     string rarity;
+    int total = 0;
 
 public:
+
     Item (string description, int value, string rarity);
     string getShortDescription();
+    string getFullDescription();
+    int getValue();
+    int getTotal();
     string getRarity();
-    int getWeight();
-    void setWeight(int weightGrams);
-    float getValue();
-    void setValue(int value);
+    void setValue(int newValue);
+    void setTotal(int newTotal);
+    void sumTotal(int value);
 };
 
 #endif /*ITEM_H_*/
