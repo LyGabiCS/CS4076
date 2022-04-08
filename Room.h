1a9f59d6fedf3acfd69b9362140bf8d56b57fbb7
Gabi <95319281+LyGabiCS@users.noreply.github.com>
2022-04-08 04:44:43 +0100

    Pre-Final Changes

--- Room.h
+++ Room.h
@@ -1,44 +1,49 @@
 #ifndef ROOM_H_
 #define ROOM_H_
-
-#include <map>
-#include <string>
-#include <vector>
 #include "item.h"
+
+#include <string>
+#include <map>
+#include <vector>
+
 using namespace std;
 using std::vector;
+using std::string;
 
-class Room {
-
+class Room
+{
 private:
-
-
-    //OR KEEP THE VECTOR ITEM BUT MAKE THE BASE CLASS BACK TO NORMAL WITH JUST A VIRTUAL FUNCTION, NOT PURE VIRTUAL FUNCTION?
     string description;
-    map<string, Room*> exits;
-    string exitString();
+    map <string, Room*> exits;
     vector <Item> itemsInRoom;
-    //vector <Common> CommonsInRoom;
-    //vector <Rare> RaresInRoom;
-    //vector <Unique> UniquesInRoom;
+
 
 
 
 public:
-    int numberOfItems();
-    Room(string description);
-    void setExits(Room* north, Room* east, Room* south, Room* west);
-    string shortDescription();
-    string longDescription();
-    Room* nextRoom(string direction);
-    //void addItem(Item* inItem);
-    void addCommon(Common* inCommon);
-    void addRare(Rare* inRare);
-    void addUnique(Unique* inUnique);
+    //DEFAULT CONSTRUCTOR
+    Room(string = "Castle");
+
+    string getDescription();
+    vector <Item> getItemsInRoom();
+    void setItemsInRoom(vector <Item>);
+    int numOfItems();
+    void setExits(Room*, Room*, Room*, Room*);
+    Room* nextRoom(string);
+    void addItem(Item*);
     string displayItem();
-    int isItemInRoom(string inString);
+    int isItemInRoom(string);
+    void takeItem(string);
     void removeItemFromRoom(int location);
+    void operator+(Item* item);
+
+
+
+
+
 
 };
 
-#endif
+
+#endif // !ROOM_H_
+
